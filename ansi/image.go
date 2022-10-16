9893893fbfc30bb9e22b0a4accc4674c285949e1
Sam <md11235@gmail.com>
2022-10-16 21:42:08 +0800

    render images using sixel

--- ansi/image.go
+++ ansi/image.go
@@ -1,8 +1,14 @@
 package ansi
 
 import (
+	"github.com/mattn/go-sixel"
+	"image"
 	"io"
+	"os"
 )
+
+import _ "image/jpeg"
+import _ "image/png"
 
 // An ImageElement is used to render images elements.
 type ImageElement struct {
@@ -24,15 +30,35 @@ func (e *ImageElement) Render(w io.Writer, ctx RenderContext) error {
 		}
 	}
 	if len(e.URL) > 0 {
-		el := &BaseElement{
-			Token:  resolveRelativeURL(e.BaseURL, e.URL),
-			Prefix: " ",
-			Style:  ctx.options.Styles.Image,
-		}
-		err := el.Render(w, ctx)
+		f, err := os.Open(e.URL)
 		if err != nil {
-			return err
+			panic(err)
 		}
+		defer f.Close()
+
+		// Calling the generic image.Decode() will tell give us the data
+		// and type of image it is as a string. We expect "png"
+		imageData, _, err := image.Decode(f)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println(imageData)
+		// fmt.Println(imageType)
+
+		// sixel.NewEncoder(os.Stdout).Encode(imageData)
+
+		sixel.NewEncoder(w).Encode(imageData)
+
+		//el := &BaseElement{
+		//	Token:  resolveRelativeURL(e.BaseURL, e.URL),
+		//	Prefix: " ",
+		//	Style:  ctx.options.Styles.Image,
+		//}
+		//err := el.Render(w, ctx)
+		//if err != nil {
+		//	return err
+		//}
+
 	}
 
 	return nil
